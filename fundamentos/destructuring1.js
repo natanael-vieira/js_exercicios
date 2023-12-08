9a64ab0cedd8799df71474f8e11ac82ed1087ccd
Natanael Vieira <natanaelsales@live.com>
2023-12-08 06:41:40 -0300

    Create destructuring1.js

---
+++ fundamentos/destructuring1.js
@@ -0,0 +1,25 @@
+// novo recurso do ES6
+
+const pessoa = {
+    nome: 'Thais',
+    idade: 36,
+    endereco: {
+        logradouro: 'Rua Bem Melhor',
+        numero: 219
+    }
+}
+
+const { nome, idade } = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade: i } = pessoa
+console.log(n, i)
+
+const {sobrenome, bemHumorada = true} = pessoa // exemplo caso não tenha este atributo na chave/valor 
+console.log(sobrenome, bemHumorada)
+
+const { endereco } = pessoa
+console.log(endereco)
+
+const { endereco: { logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep)
