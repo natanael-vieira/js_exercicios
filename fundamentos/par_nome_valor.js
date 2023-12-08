4f7d7c69cb0335ef1a964be07898a3e73e6ae43b
Natanael Vieira <natanaelsales@live.com>
2023-12-08 06:06:36 -0300

    Create par_nome_valor.js

---
+++ fundamentos/par_nome_valor.js
@@ -0,0 +1,22 @@
+const saudacao = 'Opa' // escopo 1
+
+function exec() {
+    const saudacao = 'Faaaaaaala' // escopo 2
+    return saudacao
+}
+// Tendo o mesmo nome as const, como estão em escopos diferentes não irão dar conflito
+
+// Objetos são grupos aninhados de pares chave/valor
+const cliente = {
+    nome: 'Natanael',
+    idade: 36,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 219
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
