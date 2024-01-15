7d65262c7ae46ddc1c98ab3fb29a69fc20ee0d83
Natanael Vieira <natanaelsales@live.com>
2024-01-15 05:16:32 -0300

    Update destructuring1.js

--- fundamentos/destructuring1.js
+++ fundamentos/destructuring1.js
@@ -15,7 +15,7 @@ console.log(nome, idade)
 const { nome: n, idade: i } = pessoa
 console.log(n, i)
 
-const {sobrenome, bemHumorada = true} = pessoa // exemplo caso não tenha este atributo na chave/valor 
+const {sobrenome, bemHumorada = true} = pessoa // exemplo caso não tenha este atributo na chave/valor colocar por padrão o valor true faz com que o retorno seja true e não undefined
 console.log(sobrenome, bemHumorada)
 
 const { endereco } = pessoa
@@ -23,3 +23,10 @@ console.log(endereco)
 
 const { endereco: { logradouro, numero, cep } } = pessoa
 console.log(logradouro, numero, cep)
+
+/*
+const { conta: {ag, num} } = pessoa
+console.log(ag, num)
+
+Sempre vai dar erro quando tento extrair dados de uma extrutura aninhada que não existe dentro do meu objeto, vai retornar erro, 
+diferente de eu tentar extrair de um atributo que não existe, mas que a extrutura existe, neste caso retorna como undefined*/
