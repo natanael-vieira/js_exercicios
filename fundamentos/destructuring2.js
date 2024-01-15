91202e95d0a780aaf780a1483e7eee1f6c48e390
Natanael Vieira <natanaelsales@live.com>
2024-01-15 05:50:32 -0300

    Update destructuring2.js

--- fundamentos/destructuring2.js
+++ fundamentos/destructuring2.js
@@ -1,4 +1,5 @@
-const [a] = [10]
+const [a] = [10] /* Quando vou criar ou definir um array destructuring, eu uso o colchete logo após a palavra reservada, neste caso a const.
+Caso eu vá utilizar as informações a partir de um array já pronto eu uso colchetes depois da palavra reservada: const {a}  */
 console.log(a)
 
 const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]
