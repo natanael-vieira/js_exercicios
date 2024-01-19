b7765ba19b32d423b6dd2c1ae7f0d7d91a474140
Natanael Vieira <natanaelsales@live.com>
2024-01-19 05:46:34 -0300

    Create array.js

---
+++ Array/array.js
@@ -0,0 +1,28 @@
+console.log(typeof Array, typeof new Array, typeof [])
+
+let aprovados = new Array ('Thais', 'Abigail', 'Rute')
+console.log(aprovados)
+
+aprovados = ['Thais', 'Abigail', 'Rute']
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[0])
+console.log(aprovados[3])
+
+aprovados[3] = 'Natanael'
+aprovados.push('Hector')
+
+console.log(aprovados.length)
+
+aprovados[9] = 'Amelie'
+aprovados[8] = 'Doroteia'
+console.log(aprovados.length)
+console.log(aprovados)
+console.log(aprovados[7] === undefined)
+aprovados.sort() // serve para ordenar os itens
+console.log(aprovados)
+
+delete aprovados[4]
+console.log(aprovados)
+
+aprovados = ['Thais', 'Abigail', 'Rute']
