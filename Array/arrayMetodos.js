c297299f1c91fef2025f9208927069512d71ad75
Natanael Vieira <natanaelsales@live.com>
2024-01-19 06:50:00 -0300

    Create arrayMetodos.js

---
+++ Array/arrayMetodos.js
@@ -0,0 +1,12 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // remove o último elemento do array
+console.log(pilotos)
+
+pilotos.push('Verstappen') // adiciona um novo elemento ao array
+console.log(pilotos)
+
+pilotos.shift() // removo o primeiro elemento do array
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
