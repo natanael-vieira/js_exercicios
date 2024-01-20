a812919d62891cf86b931d1515dd4efb18b61090
Natanael Vieira <natanaelsales@live.com>
2024-01-20 07:02:48 -0300

    Update foreach1.js

--- Array/foreach1.js
+++ Array/foreach1.js
@@ -1,7 +1,8 @@
 const aprovados = ['Thais', 'Abigail', 'Rute', 'Natanael']
 
-aprovados.forEach(function(nome, indice) {
+aprovados.forEach(function(nome, indice/*, array*/) {
     console.log(`${indice + 1}) ${nome}`)
+    //console.log(array)
 })
 
 aprovados.forEach(nome => console.log(nome))
