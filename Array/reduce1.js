ab94ec390553d68c1f563039d2dc75d23cf5acfa
Natanael Vieira <natanaelsales@live.com>
2024-01-25 05:36:10 -0300

    Create reduce1.js

---
+++ Array/reduce1.js
@@ -0,0 +1,15 @@
+const alunos = [
+    { nome: 'Thais', nota: 8.3, bolsista: false },
+    { nome: 'Natanael', nota: 7.2, bolsista: true },
+    { nome: 'Abigail', nota: 9.2, bolsista: false },
+    { nome: 'Rute', nota: 8.7, bolsista: false }
+]
+
+console.log(alunos.map(a => a.nota))
+
+const resultado = alunos.map(a => a.nota).reduce(function (acumulador, atual) {
+    console.log(acumulador, atual)
+    return acumulador + atual
+})
+
+console.log(resultado)
