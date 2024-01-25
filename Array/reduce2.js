6ec0cf8b6ecb4a8447f06562cf0c3038c4ff9a2a
Natanael Vieira <natanaelsales@live.com>
2024-01-25 05:43:22 -0300

    Create reduce2.js

---
+++ Array/reduce2.js
@@ -0,0 +1,11 @@
+const alunos = [
+    { nome: 'Thais', nota: 9.2, bolsista: false },
+    { nome: 'Natanael', nota: 7.3, bolsista: true },
+    { nome: 'Abigail', nota: 9.8, bolsista: false },
+    { nome: 'Rute', nota: 8.7, bolsista: true }
+]
+
+// Desafio 1: Todos os alunos são bolsistas?
+
+
+// Desafio 2: Algum aluno é bolsista?
