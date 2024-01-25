e29f64547bb2ed83756c80d9fd5ce04b0620f04c
Natanael Vieira <natanaelsales@live.com>
2024-01-25 05:43:16 -0300

    Update reduce1.js

--- Array/reduce1.js
+++ Array/reduce1.js
@@ -2,7 +2,7 @@ const alunos = [
     { nome: 'Thais', nota: 9.2, bolsista: false },
     { nome: 'Natanael', nota: 7.3, bolsista: true },
     { nome: 'Abigail', nota: 9.8, bolsista: false },
-    { nome: 'Rute', nota: 8.7, bolsista: false }
+    { nome: 'Rute', nota: 8.7, bolsista: true }
 ]
 
 console.log(alunos.map(a => a.nota))
